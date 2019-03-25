8e951a687c36a03a1a9cf606af588565b1091025
LeeX <a543748616@gmail.com>
2019-03-25 22:35:02 +0800

    调色板功能，移动端禁止滚动条

--- canvas/draw.js
+++ canvas/draw.js
@@ -9,9 +9,9 @@ function setPageSize(ele) {
   ele.height = pageHeight;
 }
 
-function drawLine(ctx, x1, y1, x2, y2, color, width) {
+function drawLine(ctx, x1, y1, x2, y2, width) {
   ctx.beginPath();
-  ctx.strokeStyle = color;
+  ctx.strokeStyle = ctx.strokeStyle ? ctx.strokeStyle : color;
   ctx.lineWidth = width;
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
@@ -19,10 +19,10 @@ function drawLine(ctx, x1, y1, x2, y2, color, width) {
   ctx.closePath();
 }
 
-function drawCircle(ctx, x, y, radius, color, deg) {
+function drawCircle(ctx, x, y, radius, deg) {
   ctx.beginPath();
   ctx.arc(x, y, radius, 0, deg);
-  ctx.fillStyle = color;
+  ctx.fillStyle = ctx.fillStyle ? ctx.fillStyle : color;
   ctx.fill();
 }
 
@@ -35,8 +35,8 @@ function listenUser(canvas) {
       turnon = true;
       if (tools.cur_tool === "brush" && turnon) {
         draw_start = { x: x, y: y };
-        drawCircle(ctx, x, y, 6, "lightblue", Math.PI * 2);
-      } else if (tools.cur_tool === "erasor" && turnon) {
+        drawCircle(ctx, x, y, 6, Math.PI * 2);
+      } else if (tools.cur_tool === "eraser" && turnon) {
         ctx.clearRect(x - 5, y - 5, 10, 10);
       }
     };
@@ -45,11 +45,11 @@ function listenUser(canvas) {
       var x = event.touches[0].clientX;
       var y = event.touches[0].clientY;
       if (tools.cur_tool === "brush" && turnon) {
-        drawCircle(ctx, x, y, 6, "lightblue", Math.PI * 2);
-        drawLine(ctx, x, y, draw_start.x, draw_start.y, "lightblue", 12);
+        drawCircle(ctx, x, y, 6, Math.PI * 2);
+        drawLine(ctx, x, y, draw_start.x, draw_start.y, 12);
         draw_start.x = x;
         draw_start.y = y;
-      } else if (tools.cur_tool === "erasor" && turnon) {
+      } else if (tools.cur_tool === "eraser" && turnon) {
         ctx.clearRect(x - 5, y - 5, 10, 10);
       }
     };
@@ -64,8 +64,8 @@ function listenUser(canvas) {
       turnon = true;
       if (tools.cur_tool === "brush" && turnon) {
         draw_start = { x: x, y: y };
-        drawCircle(ctx, x, y, 6, "lightblue", Math.PI * 2);
-      } else if (tools.cur_tool === "erasor" && turnon) {
+        drawCircle(ctx, x, y, 6, Math.PI * 2);
+      } else if (tools.cur_tool === "eraser" && turnon) {
         ctx.clearRect(x - 5, y - 5, 10, 10);
       }
     };
@@ -74,11 +74,11 @@ function listenUser(canvas) {
       var x = event.clientX;
       var y = event.clientY;
       if (tools.cur_tool === "brush" && turnon) {
-        drawCircle(ctx, x, y, 6, "lightblue", Math.PI * 2);
-        drawLine(ctx, x, y, draw_start.x, draw_start.y, "lightblue", 12);
+        drawCircle(ctx, x, y, 6, Math.PI * 2);
+        drawLine(ctx, x, y, draw_start.x, draw_start.y, 12);
         draw_start.x = x;
         draw_start.y = y;
-      } else if (tools.cur_tool === "erasor" && turnon) {
+      } else if (tools.cur_tool === "eraser" && turnon) {
         ctx.clearRect(x - 5, y - 5, 10, 10);
       }
     };
@@ -86,19 +86,20 @@ function listenUser(canvas) {
       turnon = false;
     };
   }
-
-  clear.onclick = function() {
-    width = document.documentElement.clientWidth;
-    height = document.documentElement.clientHeight;
-    ctx.clearRect(0, 0, width, height);
-  };
 }
 
 var canvas = document.getElementById("sample");
 var ctx = canvas.getContext("2d");
 var turnon = false;
 var draw_start = null;
+var brush = document.getElementById("brush");
+var eraser = document.getElementById("eraser");
 var clear = document.getElementById("clear");
+var colorpicker = document.getElementById("colorpicker");
+
+var red = document.getElementById("red");
+var lightyellow = document.getElementById("lightyellow");
+var lightgrey = document.getElementById("lightgrey");
 
 setPageSize(canvas);
 
@@ -106,11 +107,66 @@ window.onresize = function() {
   setPageSize(canvas);
 };
 
-var buttons = document.getElementById("tools").getElementsByTagName("button");
-for (var i = 0; i < buttons.length; ++i) {
-  buttons[i].onclick = function() {
-    tools.cur_tool = this.id;
-  };
-}
-
 listenUser(canvas);
+
+brush.onclick = function() {
+  tools.cur_tool = this.id;
+  if (!("active" in this.classList)) {
+    this.classList.add("active");
+    eraser.classList.remove("active");
+    clear.classList.remove("active");
+    colorpicker.style.display = "block";
+  }
+};
+
+eraser.onclick = function() {
+  tools.cur_tool = this.id;
+  if (!("active" in this.classList)) {
+    this.classList.add("active");
+    brush.classList.remove("active");
+    clear.classList.remove("active");
+    colorpicker.style.display = "none";
+  }
+};
+
+clear.onclick = function() {
+  tools.cur_tool = this.id;
+  var width = document.documentElement.clientWidth;
+  var height = document.documentElement.clientHeight;
+  ctx.clearRect(0, 0, width, height);
+  if (!("active" in this.classList)) {
+    this.classList.add("active");
+    brush.classList.remove("active");
+    eraser.classList.remove("active");
+    colorpicker.style.display = "none";
+  }
+};
+
+red.onclick = function() {
+  console.log("haha");
+  red.classList.add("active");
+  ctx.strokeStyle = "red";
+  ctx.fillStyle = "red";
+  console.log(ctx.strokeStyle);
+
+  lightyellow.classList.remove("active");
+  lightgrey.classList.remove("active");
+};
+
+lightyellow.onclick = function() {
+  lightyellow.classList.add("active");
+  ctx.strokeStyle = "lightyellow";
+  ctx.fillStyle = "lightyellow";
+
+  red.classList.remove("active");
+  lightgrey.classList.remove("active");
+};
+
+lightgrey.onclick = function() {
+  lightgrey.classList.add("active");
+  ctx.strokeStyle = "lightgrey";
+  ctx.fillStyle = "lightgrey";
+
+  red.classList.remove("active");
+  lightyellow.classList.remove("active");
+};
